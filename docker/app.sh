133412550269b7e47d71ac03c58efbb1a3f2e797
RolfAdolf <n.devrishev@gmail.com>
2023-03-24 14:49:54 +0000

    Trying get services separately

--- docker/app.sh
+++ docker/app.sh
@@ -12,7 +12,3 @@ pwd ls
 echo "STARTING SERVER"
 gunicorn src.app:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --daemon
 echo "SERVER HAS BEEN LAUNCHED"
-
-echo "STARTING CLIENT"
-gunicorn dsh.app:server --bind=0.0.0.0:8050
-echo "CLIENT HAS BEEN LAUNCHED"
