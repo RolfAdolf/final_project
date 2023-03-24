278ceaddc3eb47375b41b72ec6d7a98318d40aab
RolfAdolf <n.devrishev@gmail.com>
2023-03-24 15:31:06 +0000

    Rollback

--- docker/app.sh
+++ docker/app.sh
@@ -2,13 +2,14 @@
 
 cd src
 echo "UPGRADING ALEMBIC"
-pwd
-ls
 alembic upgrade head
 cd ../
 echo "UPGRADING has been finished"
-pwd ls
 
 echo "STARTING SERVER"
-gunicorn src.app:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+gunicorn src.app:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --daemon
 echo "SERVER HAS BEEN LAUNCHED"
+
+echo "STARTING CLIENT"
+gunicorn dsh.app:server --bind=0.0.0.0:8050
+echo "CLIENT HAS BEEN LAUNCHED"
